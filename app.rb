e036dea1caec260dcb994135c2d878a3b1f61c12
weathermaker <zebesquawz@gmail.com>
2016-08-03 17:12:52 -0400

    Update app.rb

--- app.rb
+++ app.rb
@@ -9,6 +9,15 @@ get "/" do
   erb :home
 end
 
+class Contact < ActiveRecord::Base
+  self.table_name = 'salesforce.contact'
+end
+
+get "/contacts" do
+  @contacts = Contact.all
+  erb :index
+end
+
 
 class Contact < ActiveRecord::Base
   self.table_name = 'salesforce.contact'
